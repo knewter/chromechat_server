0ffea684375ec94c63f3ab3adc9dee590a9bc7c4
Josh Adams <josh@isotope11.com>
2013-11-09 16:45:02 -0600

    Add broadcasting of part, join; sending

--- lib/chromechat_server/channel.ex
+++ lib/chromechat_server/channel.ex
@@ -6,6 +6,19 @@ defmodule ChromechatServer.Channel do
     :gen_server.start_link(__MODULE__, ChromechatServer.ChannelState.new, [])
   end
 
+  def join(pid, user) do
+    :gen_server.call(pid, {:join, user})
+  end
+
+  def part(pid) do
+    :gen_server.call(pid, :part)
+  end
+
+  # GenServer API
+  def init(state) do
+    {:ok, state}
+  end
+
   def handle_call({:join, user}, from, state) do
     {status, new_state} = add_user(user, from, state)
     broadcast_join_message(user.username, new_state)
@@ -15,10 +28,12 @@ defmodule ChromechatServer.Channel do
     usernames = Enum.map(state.listeners, fn(user) -> user.username end)
     {:reply, usernames, state}
   end
-
-  # GenServer API
-  def init(state) do
-    {:ok, state}
+  def handle_call(:part, from, state) do
+    {from_pid, _} = from
+    user = user_from_pid(from_pid, state)
+    broadcast_part_message(user.username, state)
+    new_state = remove_user(from, state)
+    {:reply, :ok, new_state}
   end
 
   # Private bits
@@ -29,12 +44,22 @@ defmodule ChromechatServer.Channel do
     end
   end
 
+  defp remove_user({from_pid, _ref}, state) do
+    new_listeners = state.listeners |> Enum.filter(fn(user) -> user.pid != from_pid end)
+    state.listeners(new_listeners)
+  end
+
   defp has_user?(username, state) do
     Enum.any?(state.listeners, fn(user) -> user.username == username end)
   end
 
   defp broadcast_join_message(username, state) do
     message = build_message("#{username} has joined.")
+    broadcast(message, state)
+  end
+
+  defp broadcast_part_message(username, state) do
+    message = build_message("#{username} has parted.")
     broadcast(message, state)
   end
 
@@ -47,5 +72,9 @@ defmodule ChromechatServer.Channel do
 
   defp build_message(message_text) do
     ChromechatServer.Message.new(username: "system", text: message_text)
+  end
+
+  defp user_from_pid(from_pid, state) do
+    state.listeners |> Enum.find(fn(user) -> from_pid == user.pid end)
   end
 end
--- test/unit/channel_test.exs
+++ test/unit/channel_test.exs
@@ -18,6 +18,27 @@ defmodule ChromechatServer.ChannelTest do
     assert {:reply, ["knewter"], state_with_user} == ChromechatServer.Channel.handle_call(:nicklist, { meta[:self_pid], [] }, state_with_user )
   end
 
+  test "A client can send messages.", meta do
+    knewter = new_user(meta[:self_pid])
+    state_with_user = ChromechatServer.ChannelState.new(listeners: [knewter])
+    assert {:noreply, state_with_user} == ChromechatServer.Channel.handle_cast({:send, meta[:self_pid], "some message"}, state_with_user)
+  end
+
+  test "A client joins and is announced to all attached listeners.", meta do
+    {:ok, pid} = ChromechatServer.Channel.start_link
+    ChromechatServer.Channel.join(pid, new_user(meta[:self_pid]))
+    expected_message = ChromechatServer.Message.new(username: "system", text: "knewter has joined.")
+    assert_received expected_message
+  end
+
+  test "A client parts and is announced to all attached listeners.", meta do
+    {:ok, pid} = ChromechatServer.Channel.start_link
+    ChromechatServer.Channel.join(pid, new_user(meta[:self_pid]))
+    ChromechatServer.Channel.part(pid)
+    expected_message = ChromechatServer.Message.new(username: "system", text: "knewter has parted.")
+    assert_received expected_message
+  end
+
   def new_user(pid) do
     ChromechatServer.User.new(username: "knewter", pid: pid)
   end
